d1aeeb67954cd102be53843493c41d02f7694d3b
Jakub Wodecki <wodecki@atlasgroup.cz>
2023-01-06 12:51:32 +0100

    Added validation of habit values and error borders

--- app/controllers/Detail.php
+++ app/controllers/Detail.php
@@ -19,6 +19,15 @@ class Detail extends Controller
         $password = "";
         $db = "habitjournal";
 
+        $data = [
+            'id' => $_SESSION['id'],
+            'name' => htmlspecialchars($_POST['habit-name']),
+            'name_abbr' => htmlspecialchars($_POST['habit-abbr']),
+            'color' => htmlspecialchars($_POST['habit-color']),
+            'description' => htmlspecialchars($_POST['habit-desc'])
+            ];
+        $invalid = $this->validateInput($data);
+
         $conn = new mysqli($servername, $username, $password, $db);
 
         if ($conn->connect_error)
@@ -26,13 +35,53 @@ class Detail extends Controller
             die();
         }
 
-        //TODO check whether we hve everything
-        $saveSql = "INSERT INTO habits VALUES ('". uniqid() . "', '". $_SESSION['id'] . "', '" .
-            $_POST['habit-name'] . "', '" . $_POST['habit-abbrev'] . "', '" . $_POST['habit-color'] . "', '" .
-            $_POST['habit-desc'] . "')";
-        $conn->query($saveSql);
-        $conn->close();
+        if (count($invalid) == 0)
+        {
+            $checkSql = "SELECT * FROM habits WHERE name_abbr = '" . $data['habit-abbr'] ."'";
 
-        $this->view('habitlist');
+            $results = $conn->query($checkSql);
+
+            if ($results->num_rows != 0)
+            {
+                $data['abbr-invalid'] = true;
+                $this->view('habitdetail', $data);
+            } else
+            {
+
+                $saveSql = "INSERT INTO habits VALUES ('". $data['id'] . "', '" .
+                    $data['name'] . "', '" . $data['name_abbr'] . "', '" . $data['color'] . "', '" .
+                    $data['description'] . "')";
+
+                $conn->query($saveSql);
+                $this->view('habitlist');
+            }
+        } else
+        {
+            $this->view('habitdetail', array_merge($data, $invalid));
+        }
+
+        $conn->close();
+    }
+
+    public function validateInput($data)
+    {
+        $invalid = [];
+
+        if ($data['name'] == '')
+        {
+            $invalid['name-invalid'] = true;
+        }
+
+        if ($data['name_abbr'] == '')
+        {
+            $invalid['abbr-invalid'] = true;
+        }
+
+        if ($data['color'] == '')
+        {
+            $invalid['color-invalid'] = true;
+        }
+
+        return $invalid;
     }
 }
