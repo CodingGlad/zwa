a5de43d020ced844648a07efb3735b504be8d443
Jakub Wodecki <wodecki@atlasgroup.cz>
2023-01-10 00:06:20 +0100

    added logout documentation

--- app/controllers/LogOut.php
+++ app/controllers/LogOut.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+ * LogOut controller handles logging out of a user via url.
+ */
 class LogOut extends Controller
 {
+    /**
+     * This method is used for logging out of currently signed in user.
+     * @return void
+     */
     public function index()
     {
         unset($_SESSION['id']);
