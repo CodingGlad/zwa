47b16f1fbd273da8ea280887a1b0091d3f09957b
Jakub Wodecki <wodecki@atlasgroup.cz>
2023-01-12 18:54:13 +0100

    refactored App.php to make it more readable

--- app/core/App.php
+++ app/core/App.php
@@ -5,6 +5,9 @@
  */
 class App
 {
+    const DEFAULT_SIGNED_IN = 'calendar';
+    const DEFAULT_NOT_SIGNED_ID = 'signin';
+    const DEFAULT_METHOD = 'index';
     protected $controller = 'signin';
     protected $method = 'index';
     protected $params = [];
@@ -18,47 +21,13 @@ class App
 
         $url = $this->parseUrl();
 
-        if (isset($_SESSION['id']) && $this->isIdValid($_SESSION['id'])) {
-            if (isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php'))
-            {
-                if ($url[0] == 'signin' || $url[0] == 'signup')
-                {
-                    $this->controller = 'calendar';
-                } else
-                {
-                    $this->controller = $url[0];
-                }
-                unset($url[0]);
-            } else if (count($url) == 0)
-            {
-                $this->controller = 'calendar';
-            }
-        } else
-        {
-            if (isset($url[0]) && ($url[0] == 'signin' || $url[0] == 'signup'))
-            {
-                $this->controller = $url[0];
-                unset($url[0]);
-            } else
-            {
-                $this->controller = 'signin';
-            }
-        }
+        $this->chooseValidController($url);
 
         require_once '../app/controllers/' . $this->controller . '.php';
 
         $this->controller = new $this->controller;
 
-        if (isset($url[1]) && method_exists($this->controller, $url[1]))
-        {
-            if (($this->controller == 'signin' || $this->controller == 'signup') && isset($_SESSION['id']))
-            {
-                $this->controller = 'index';
-                unset($url[1]);
-            }
-            $this->method = $url[1];
-            unset($url[1]);
-        }
+        $this->chooseValidMethod($url);
 
         $this->params = $url ? array_values($url) : [];
 
@@ -108,6 +77,62 @@ class App
         } else
         {
             return false;
+        }
+    }
+
+    private function chooseValidController(&$url)
+    {
+        if (isset($_SESSION['id']) && $this->isIdValid($_SESSION['id']))
+        {
+            $this->signedInUser($url);
+        } else
+        {
+            $this->unknownUser($url);
+        }
+    }
+
+    private function signedInUser(&$url)
+    {
+        if (isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php'))
+        {
+            if ($url[0] == 'signin' || $url[0] == 'signup')
+            {
+                $this->controller = self::DEFAULT_SIGNED_IN;
+            } else
+            {
+                $this->controller = $url[0];
+            }
+            unset($url[0]);
+        } else if (count($url) == 0)
+        {
+            $this->controller = self::DEFAULT_SIGNED_IN;
+        }
+    }
+
+    private function unknownUser(&$url)
+    {
+        if (isset($url[0]) && ($url[0] == 'signin' || $url[0] == 'signup'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        } else
+        {
+            $this->controller = self::DEFAULT_NOT_SIGNED_ID;
+        }
+    }
+
+    private function chooseValidMethod(&$url)
+    {
+        if (isset($url[1]) && method_exists($this->controller, $url[1]))
+        {
+            if (($this->controller == 'signin' || $this->controller == 'signup') &&
+                (isset($_SESSION['id']) && $this->isIdValid($_SESSION['id'])))
+            {
+                $this->controller = self::DEFAULT_METHOD;
+                unset($url[1]);
+            }
+            $this->method = $url[1];
+            unset($url[1]);
         }
     }
 }
